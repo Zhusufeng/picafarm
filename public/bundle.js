ab5be9ec3d5d46f94dcd9fa82491bdded0ee6fbb
Kevin Su <kevinsu1212@gmail.com>
2017-08-15 12:58:52 -0700

    updated farmer page css

--- public/bundle.js
+++ public/bundle.js
@@ -23,7 +23,6 @@ angular.module('picafarm').component('farmerSearch', {
 
 });
 "use strict";
-"use strict";
 'use strict';
 
 angular.module('picafarm').controller('mainCtrl', ["$scope", "$http", function ($scope, $http) {
@@ -87,4 +86,5 @@ angular.module('picafarm').controller('mainCtrl', ["$scope", "$http", function (
     return _name = newName;
   };
 }]);
+"use strict";
 //# sourceMappingURL=bundle.js.map
