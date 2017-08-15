086ead7a023806227c749edbf0c107db08d7bcb6
A9Sharma <anjalisharma0626@gmail.com>
2017-08-15 10:48:23 -0700

    Hide farm list

--- public/bundle.js
+++ public/bundle.js
@@ -23,6 +23,7 @@ angular.module('picafarm').component('farmerSearch', {
 
 });
 "use strict";
+"use strict";
 'use strict';
 
 angular.module('picafarm').controller('mainCtrl', ["$scope", "$http", function ($scope, $http) {
@@ -84,5 +85,4 @@ angular.module('picafarm').controller('mainCtrl', ["$scope", "$http", function (
     return _name = newName;
   };
 }]);
-"use strict";
 //# sourceMappingURL=bundle.js.map
