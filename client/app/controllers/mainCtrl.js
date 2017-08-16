0ff9bdf82312dbf02e7cd50386c5cbe7d5b0751d
kevinlabs <kevinlabx@gmail.com>
2017-08-16 14:15:49 -0500

    Testing page trigger

--- client/app/controllers/mainCtrl.js
+++ client/app/controllers/mainCtrl.js
@@ -1,61 +1,32 @@
-angular.module('picafarm').controller('mainCtrl', function($scope, $http) {
+angular.module('picafarm').controller('mainCtrl', function ($scope, $http) {
 
-  // this.farmers = {};
   $scope.farmerArr = [];
-  $scope.tester = '!!! test !!!';
 
-  $scope.selectedFarm = {};
+  // $scope.farmerSearchPage = true;
+  // $scope.loginPage = false;
+  // $scope.signUpPage = false;
 
-  $scope.get = function() {
-    $http.get("/search").then(function(response) {
+  $scope.pages = {
+    farmerSearchPage: false,
+    loginPage: false,
+    signUpPage: false
+  };
+
+  $scope.get = function () {
+    $http.get("/search").then(function (response) {
       console.log(response.data);
       $scope.farmerArr = response.data;
-    }).catch(function(response) {
+    }).catch(function (response) {
       console.log(response);
     });
   };
 
-  $scope.hideComponent = function() {
-    var elements = document.getElementsByClassName('farms');
-    // if (elements.classList.contains('show')) {
-    //   elements.classList.remove('show');
-    // }
-    // elements.className += 'hide';
-    console.log('Logging what elements is here: ', elements);
-    console.log('Logging type of elements: ', Array.isArray(elements));
+  $scope.pageChanger = (val) => {
+    if (val) {
 
-    for (var i = 0; i < elements.length; i += 1) {
-      console.log('Logging what elements classList is here: ', elements[i].classList);
-      elements[i].classList.toggle('hide');
     }
-
-    //console.log('Logging what elements classList is here: ',$scope.elements.classList);
-   // elements.classList.toggle('hide');
-  };
-
-  $scope.showComponent = function() {
-    var elements = document.getElementsByClassName('farms');
-    // if (elements.classList.contains('hide')) {
-    //   elements.classList.remove('hide');
-    // }
-    //elements.className += 'show';
-    elements.classList.toggle('show');    
   };
 
   $scope.get();
 
-  var _name = 'Brian';
-  $scope.user = {
-    name: function(newName) {
-     // Note that newName can be undefined for two reasons:
-     // 1. Because it is called as a getter and thus called with no arguments
-     // 2. Because the property should actually be set to undefined. This happens e.g. if the
-     //    input is invalid
-     return arguments.length ? (_name = newName) : _name;
-    }
-  };
-
-  $scope.grabFarmTitle = function(newName) {
-    return _name = newName;
-  };
 });
--- public/bundle.js
+++ public/bundle.js
@@ -5,11 +5,17 @@ angular.module('picafarm', []).config(function () {});
 
 angular.module('picafarm').controller('mainCtrl', ["$scope", "$http", function ($scope, $http) {
 
-  // this.farmers = {};
   $scope.farmerArr = [];
-  $scope.tester = '!!! test !!!';
 
-  $scope.selectedFarm = {};
+  // $scope.farmerSearchPage = true;
+  // $scope.loginPage = false;
+  // $scope.signUpPage = false;
+
+  $scope.pages = {
+    farmerSearchPage: false,
+    loginPage: false,
+    signUpPage: false
+  };
 
   $scope.get = function () {
     $http.get("/search").then(function (response) {
@@ -20,49 +26,11 @@ angular.module('picafarm').controller('mainCtrl', ["$scope", "$http", function (
     });
   };
 
-  $scope.hideComponent = function () {
-    var elements = document.getElementsByClassName('farms');
-    // if (elements.classList.contains('show')) {
-    //   elements.classList.remove('show');
-    // }
-    // elements.className += 'hide';
-    console.log('Logging what elements is here: ', elements);
-    console.log('Logging type of elements: ', Array.isArray(elements));
-
-    for (var i = 0; i < elements.length; i += 1) {
-      console.log('Logging what elements classList is here: ', elements[i].classList);
-      elements[i].classList.toggle('hide');
-    }
-
-    //console.log('Logging what elements classList is here: ',$scope.elements.classList);
-    // elements.classList.toggle('hide');
-  };
-
-  $scope.showComponent = function () {
-    var elements = document.getElementsByClassName('farms');
-    // if (elements.classList.contains('hide')) {
-    //   elements.classList.remove('hide');
-    // }
-    //elements.className += 'show';
-    elements.classList.toggle('show');
+  $scope.pageChanger = function (val) {
+    if (val) {}
   };
 
   $scope.get();
-
-  var _name = 'Brian';
-  $scope.user = {
-    name: function name(newName) {
-      // Note that newName can be undefined for two reasons:
-      // 1. Because it is called as a getter and thus called with no arguments
-      // 2. Because the property should actually be set to undefined. This happens e.g. if the
-      //    input is invalid
-      return arguments.length ? _name = newName : _name;
-    }
-  };
-
-  $scope.grabFarmTitle = function (newName) {
-    return _name = newName;
-  };
 }]);
 "use strict";
 'use strict';
@@ -83,6 +51,14 @@ angular.module('picafarm').component('farmerPage', {
 angular.module('picafarm').component('farmerSearch', {
 
   templateUrl: 'views/farmerSearch.html',
+  controller: 'mainCtrl'
+
+});
+'use strict';
+
+angular.module('picafarm').component('login', {
+
+  templateUrl: 'views/login.html',
   controller: 'mainCtrl'
 
 });
