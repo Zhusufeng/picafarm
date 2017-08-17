283918cde335d9d917f6bd1305e52c96547f63d7
Lisa Gee <ZhuSuFeng@gmail.com>
2017-08-17 09:54:43 -0700

    Use service to check session

--- client/app/services/mainService.js
+++ client/app/services/mainService.js
@@ -1,16 +1,16 @@
 angular.module('picafarm').service('mainService', function ($http, $rootScope) {
-  
+
   this.createAccount = (user) => {
-        console.info('Creating this user from createAccount-Service: ', user);
-        return $http({
-                method: 'POST',
-                url: '/register/account/create',
-                data: user
-            })
-            .then((response) => {
-                self.checkSessions();
-            });
-    };
+    console.info('Creating this user from createAccount-Service: ', user);
+    return $http({
+      method: 'POST',
+      url: '/register/account/create',
+      data: user
+    })
+      .then((response) => {
+        self.checkSessions();
+      });
+  };
 
   //endpoint: /user/signup
 
