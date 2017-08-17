b2ebf405134391e96435f08ddfbf2b68c76c0b49
Lisa Gee <ZhuSuFeng@gmail.com>
2017-08-17 10:05:40 -0700

    Update with messages of future functionality

--- client/app/controllers/userCtrl.js
+++ client/app/controllers/userCtrl.js
@@ -1,4 +1,14 @@
-angular.module('picafarm').controller('userCtrl', function ($scope) {
+angular.module('picafarm').controller('userCtrl', function ($scope, mainService) {
+
+  $scope.createUser = (user) => {
+    console.log('Here is the user: ', user);
+    mainService.createAccount(user);
+
+    // Feedback to user
+    alert('You created an account');
+
+    // Feedback if error ...
+  };
 
   //endpoint: /user/signup
 
